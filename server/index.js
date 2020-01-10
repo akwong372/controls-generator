9a732d23350c56379de671736279bc85adf76df9
Steven Santos <stevensantos@Stevens-MacBook-Pro-2.local>
2020-01-09 17:44:44 -0800

    init && configured express

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.use('/', () => console.log('Testing'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Controls Generator is listening on ${PORT}`);
+});
