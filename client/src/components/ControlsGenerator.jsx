04f839f267f85a155784658c0ffb7903001cd616
Steven Santos <stevensantos@Stevens-MacBook-Pro-2.local>
2020-01-09 18:01:50 -0800

    init first component

---
+++ client/src/components/ControlsGenerator.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import axios from 'axios';
+
+class ControlsGenerator extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Controls Generator</h1>
+      </div>
+    )
+  }
+}
+
+export default ControlsGenerator
