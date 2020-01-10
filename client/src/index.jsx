e701a68e9f80fd4bbfe558e5454f2a61aab60cf6
Steven Santos <stevensantos@Stevens-MacBook-Pro-2.local>
2020-01-09 18:00:34 -0800

    init ReactDOM

---
+++ client/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ControlsGenerator from './components/ControlsGenerator.jsx';
+
+ReactDOM.render(<ControlsGenerator />, document.getElementById('controls-generator'));
